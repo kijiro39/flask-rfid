a3c7af7f6a9fb349b5153bd76ef0323bbdfc15c6
Nor Aiman <134761830+kijiro39@users.noreply.github.com>
2023-06-29 03:52:02 +0800

    Update __init__.py

--- project/website/__init__.py
+++ project/website/__init__.py
@@ -16,7 +16,8 @@ n = 'projectFYP'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'bzdfbsdfbwbreb'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:@localhost:3306/projectfyp'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://admin:v1GxmS8FFZ0mag8WKiF2uAkLKAC3qiXq@dpg-cie7jt59aq0ce3bav0rg-a.singapore-postgres.render.com/projectfyp'
+    # f'mysql+pymysql://root:@localhost:3306/projectfyp'
     app.config['MESSAGE_FLASHING_OPTIONS'] = {'duration': 5}
     db.init_app(app)
         
